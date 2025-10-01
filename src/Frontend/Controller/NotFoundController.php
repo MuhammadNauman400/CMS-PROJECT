f3e548136f93ba46d38aaadf1a4aa90b8346be9c
MuhammadNauman400 <naumanmuhammad663@gmail.com>
2025-10-01 12:03:32 +0500

    Completed

---
+++ src/Frontend/Controller/NotFoundController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Frontend\Controller;
+
+class NotFoundController extends AbstractController
+{
+
+    public function error404(){
+        return parent::error404();
+    }
+   
+}
---
+++ src/Model/PageModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Model;
+
+class PageModel {
+    public string $id;
+    public string $slug;
+    public string $title;
+    public string $content;
+}
